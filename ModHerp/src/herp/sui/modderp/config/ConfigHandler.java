cbb73ba700f5950f74b62fb167d15c76cc1afcf2
suicune_dude <suicunedude@gmail.com>
2014-02-25 11:29:17 +0000

    Now the configs are ruled by the iron fist of global definition.

--- ModHerp/src/herp/sui/modderp/config/ConfigHandler.java
+++ ModHerp/src/herp/sui/modderp/config/ConfigHandler.java
@@ -1,5 +1,7 @@
 package herp.sui.modderp.config;
 
+import herp.sui.modderp.lib.ConfigValues;
+
 import java.io.File;
 
 import net.minecraftforge.common.Configuration;
@@ -12,8 +14,10 @@ public class ConfigHandler {
 		
 		config.load();
 		
-		int test = config.get("useless stuff", "herp", 5).getInt();
-		String test2 = config.get("ueseless stuff", "Some text", "WHAT DO YOU WANT FROM ME!?").getString();
+		int test = config.get(ConfigValues.CATEGORY_USELESS, ConfigValues.HERP_NAME, 
+				ConfigValues.HERP_DEFAULT).getInt();
+		String test2 = config.get(ConfigValues.CATEGORY_USELESS, ConfigValues.SOMETEXT_NAME,
+				ConfigValues.SOMETEXT_DEFAULT).getString();
 		
 		config.save();
 		
---
+++ ModHerp/src/herp/sui/modderp/lib/ConfigValues.java
@@ -0,0 +1,13 @@
+package herp.sui.modderp.lib;
+
+public class ConfigValues {
+	
+	public static final String CATEGORY_USELESS = "useless stuff";
+	
+	public static final String HERP_NAME = "herp";
+	public static final int HERP_DEFAULT = 5;
+	
+	public static final String SOMETEXT_NAME = "Some text";
+	public static final String SOMETEXT_DEFAULT = "WHAT DO YOU WANT FROM ME!?";
+
+}
