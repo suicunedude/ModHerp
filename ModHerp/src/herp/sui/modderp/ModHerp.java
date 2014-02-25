16d2c7232003695a07893fe48cf33f53218dbc4a
suicune_dude <suicunedude@gmail.com>
2014-02-25 10:17:13 +0000

    Made skeleton of mod. Less arrows than a normal skeleton

---
+++ ModHerp/src/herp/sui/modderp/ModHerp.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package herp.sui.modderp;
+
+import cpw.mods.fml.common.Mod;
+
+/**
+ * @author suicune_dude
+ *
+ */
+@Mod (modid = "HERP", name = "Herp", version = "Whatever you want")
+public class ModHerp {
+
+}
